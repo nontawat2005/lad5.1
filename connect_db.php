874578eea15b44f83ec55212e128afc9d454f10b
nontawat <nontawat.do@ku.th>
2025-08-29 16:24:40 +0700

    first commit

---
+++ connect_db.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "nontawat";
+$password = "Sam_140648";
+$db = "nontawat";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $db);
+// Check connection
+if (!$conn) {
+die("Connection failed: " . mysqli_connect_error());
+}
+//echo "Connected successfully";
+?>
