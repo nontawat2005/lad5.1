da3e29e4b89eb9599461179d01bea122176c31e2
nontawat <nontawat.do@ku.th>
2025-10-26 13:51:00 +0700

    ,

--- connect_db.php
+++ connect_db.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "nontawat";
-$password = "Sam_140648";
+$password = "Yes";
 $db = "nontawat";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $db);
